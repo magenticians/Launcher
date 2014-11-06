714efd1ef4432285f330e062be297e1bd90157fb
Elias <mail@eliaszerrouq.nl>
2014-11-06 10:23:15 +0000

    Add configuration launcher items

--- src/Magenticians/Launcher/Block/LauncherItems.php
+++ src/Magenticians/Launcher/Block/LauncherItems.php
@@ -5,17 +5,14 @@ class LauncherItems extends \Magento\Backend\Block\Template
 {
     const ITEMS_SEPARATOR = ' - ';
 
+    const CONFIG_ITEMS_PREFIX = 'Configuration - ';
+
     protected $_template = 'launcher.phtml';
 
     /**
-     * @var \Magento\Backend\Model\Menu\Filter\IteratorFactory
+     * @var \Magenticians\Launcher\Helper\Data
      */
-    protected $_iteratorFactory;
-
-    /**
-     * @var \Magento\Backend\Block\Menu
-     */
-    protected $_blockMenu;
+    protected $_dataHelper;
 
     /**
      * @var \Magento\Backend\Model\UrlInterface
@@ -24,85 +21,31 @@ class LauncherItems extends \Magento\Backend\Block\Template
 
     /**
      * @param \Magento\Backend\Block\Template\Context $context
-     * @param \Magento\Backend\Model\Menu\Filter\IteratorFactory $iteratorFactory
-     * @param \Magento\Backend\Block\Menu $blockMenu
+     * @param \Magenticians\Launcher\Helper\Data $dataHelper
+     * @param \Magento\Backend\Model\UrlInterface $url
      * @param array $data
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        \Magento\Backend\Model\Menu\Filter\IteratorFactory $iteratorFactory,
-        \Magento\Backend\Block\Menu $blockMenu,
+        \Magenticians\Launcher\Helper\Data $dataHelper,
         \Magento\Backend\Model\UrlInterface $url,
-        array $data
+        array $data = array()
     ) {
-        $this->_iteratorFactory = $iteratorFactory;
-        $this->_blockMenu = $blockMenu;
-        $this->_url = $url;
         parent::__construct($context, $data);
-    }
-
-    /**
-     * Get menu config model
-     *
-     * @return \Magento\Backend\Model\Menu
-     */
-    public function getMenuModel()
-    {
-        return $this->_blockMenu->getMenuModel();
-    }
-
-    /**
-     * Get menu filter iterator
-     *
-     * @param \Magento\Backend\Model\Menu $menu
-     * @return \Magento\Backend\Model\Menu\Filter\Iterator
-     */
-    protected function getMenuIterator($menu)
-    {
-        return $this->_iteratorFactory->create(array('iterator' => $menu->getIterator()));
-    }
-
-    /**
-     * Recursively iterate through the menu model
-     * @param $menu
-     * @param array $result
-     * @param string $fullName
-     * @return array
-     */
-    public function getMenuArray($menu, & $result = array(), $fullName = '')
-    {
-        if (! empty($fullName)) {
-            $fullName .= self::ITEMS_SEPARATOR;
-        }
-
-        foreach ($this->getMenuIterator($menu) as $menuItem) {
-            /** @var $menuItem \Magento\Backend\Model\Menu\Item  */
-
-            if ($menuItem->getUrl() !== '#') {
-                // Only add meaningful entries
-                $result[] = array(
-                    'value' => $menuItem->getUrl(),
-                    'label' => $fullName . $menuItem->getTitle()
-                );
-            }
-
-            if ($menuItem->hasChildren()) {
-                $this->getMenuArray($menuItem->getChildren(), $result, $fullName . $menuItem->getTitle());
-            }
-        }
-
-        return $result;
+        $this->_dataHelper = $dataHelper;
+        $this->_url = $url;
     }
 
     /**
      * Transform $this->getMenuArray() into a JSON array to be fed to ui.autocomplete
-     * Applies a bit of trickery to parse the secret form keys properly
      * @return string
      */
-    public function getMenuJson()
+    public function getItemsJson()
     {
-        $menuArray = $this->getMenuArray($this->getMenuModel());
-        return json_encode($menuArray, JSON_UNESCAPED_SLASHES);
+        $menuArray = $this->_dataHelper->getMenuArray($this->_dataHelper->getMenuModel(), self::ITEMS_SEPARATOR);
+        $configSectionsArray = $this->_dataHelper->getConfigSectionsArray(self::ITEMS_SEPARATOR, self::CONFIG_ITEMS_PREFIX);
+
+        return json_encode(array_merge($menuArray, $configSectionsArray), JSON_UNESCAPED_SLASHES);
     }
 
     /**
